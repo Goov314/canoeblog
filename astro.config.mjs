e5cd9aabe06917c737d6401df4ecd23626b1eeb2
Goov314 <84781801+Goov314@users.noreply.github.com>
2026-05-08 17:18:42 -0400

    fixed pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://Goov314.github.io',
+	base: '/canoeblog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
